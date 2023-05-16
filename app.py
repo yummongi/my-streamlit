c376877d34c17400e494ec8482a2a937cc861d87
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-16 11:25:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ st.subheader("this is the subheader")
 st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r''' a + a r ^1+ a r^2 + a r^3 ''')
+
+st.sidebar.title("This is written inside the sidbar")
+st.sidebar.button("Click")
+st.sidbar.radio("Pick your gender", ["Male", "Female"] )
