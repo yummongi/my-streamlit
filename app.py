3eb9a3eabd7e10360e13c3ddf0f62857bfc779f8
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-16 11:15:06 +0900

    Update app.py
    update Basic Text elements

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 x = st.slider('Select a value')
 st.write(x, 'sqaured is', x * x)
+
+st.title("this is the app title")
+st.header("this is the header")
+st.markdown("this is the markdown")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a + a r ^1+ a r^2 + a r^3 ''')
