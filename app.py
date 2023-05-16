4ee4aaffaa6681be3b0627e07de2ccf5c4d25677
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-16 11:32:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
 x = st.slider('Select a value')
 st.write(x, 'sqaured is', x * x)
@@ -13,3 +15,10 @@ st.latex(r''' a + a r ^1+ a r^2 + a r^3 ''')
 
 st.sidebar.title("This is written inside the sidbar")
 st.sidebar.button("Click")
+
+#bar chart
+rand = np.random.normal(1,2,size=20)
+fig,ax = plt.subplots()
+ax.hist(rand, bins = 15)
+st.pyplot(fig)
+
