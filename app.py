7feff89927a48b41c5c8058ace534dcb10cddb12
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-18 22:09:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,11 +29,12 @@ if st.checkbox('정류장 간 거리를 계산하시겠습니까?'):
     city_to_calculate_2 = st.selectbox("두 번째 도시를 선택하세요.", data['도시명'].unique())
     bus_stop_2 = st.selectbox('두 번째 도시의 정류장을 선택하세요.', data[data['도시명'] == city_to_calculate_2]['정류장명'].unique())
 
-    location_1 = data[(data['정류장명'] == bus_stop_1) & (data['도시명'] == city_to_calculate_1)][['latitude', 'longitude']].values[0]
-    location_2 = data[(data['정류장명'] == bus_stop_2) & (data['도시명'] == city_to_calculate_2)][['latitude', 'longitude']].values[0]
+    location_1 = data[(data['정류장명'] == bus_stop_1) & (data['도시명'] == city_to_calculate_1)][['longitude', 'latitude']].values[0]
+    location_2 = data[(data['정류장명'] == bus_stop_2) & (data['도시명'] == city_to_calculate_2)][['longitude', 'latitude']].values[0]
 
-    gs = gpd.GeoSeries([Point(location_1), Point(location_2)])
-    distance = gs.distance(gs.shift()).values[1] / 1000 #킬로미터 계산
+    gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy([location_1[0], location_2[0]], [location_1[1], location_2[1]]))
+    distance = gdf.geometry.distance(gdf.shift()).values[1]
+
     st.write(f"{city_to_calculate_1}의 {bus_stop_1}과(와) {city_to_calculate_2}의 {bus_stop_2} 사이의 거리는 {distance:.2f} km 입니다.")
 
 st.subheader('도시별 정류장 수')
