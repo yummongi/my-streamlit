6d2fe0118838d51b4c0a11ddfbb2868548d7ff29
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-18 20:54:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,52 +2,51 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = st.slider('Select a value')
-st.write(x, 'sqaured is', x * x)
-
-st.title("this is the app title")
-st.header("this is the header")
-st.markdown("this is the markdown")
-st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r''' a + a r ^1+ a r^2 + a r^3 ''')
-
-st.sidebar.title("This is written inside the sidbar")
-st.sidebar.button("Click")
-
-#bar chart
-rand = np.random.normal(1,2,size=20)
-fig,ax = plt.subplots()
-ax.hist(rand, bins = 15)
-st.pyplot(fig)
-
-#add data
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# 데이터를 로드합니다.
+# CSV 파일에서 데이터를 불러옵니다.
 @st.cache
 def load_data():
-    data = pd.read_csv('대기질_데이터.csv') # 이 부분은 실제 데이터 파일의 경로와 일치해야 합니다.
+    data = pd.read_csv('bus_stop.csv')
     return data
 
 data = load_data()
 
-# 앱의 타이틀을 설정합니다.
-st.title('대한민국 대기질 정보')
+st.title('대한민국 버스 정류장 정보 조회')
 
-# 사용자로부터 도시와 오염 물질을 선택하도록 합니다.
-city = st.selectbox('도시를 선택하세요.', data['도시'].unique())
-pollutant = st.selectbox('오염 물질을 선택하세요.', ['미세먼지', '초미세먼지', '이산화질소', '오존'])
+# 사용자로부터 도시명을 입력받기
+cities = data['도시명'].unique()
+city = st.selectbox('도시를 선택하세요.', cities)
 
-# 선택된 도시와 오염 물질에 대한 데이터를 필터링합니다.
-filtered_data = data[(data['도시'] == city) & (data['오염 물질'] == pollutant)]
+# 선택된 도시에 해당하는 정보를 필터
+filtered_data = data[data['도시명'] == city]
 
-# 필터링된 데이터의 통계를 보여줍니다.
-st.write(filtered_data.describe())
+# 사용자로부터 정류장명을 입력
+bus_stops = filtered_data['정류장명'].unique()
+bus_stop = st.selectbox('정류장을 선택하세요.', bus_stops)
 
-# 필터링된 데이터를 차트로 보여줍니다.
-st.line_chart(filtered_data['농도'])
+# 선택된 도시와 정류장에 해당하는 정보 필터
+selected_data = filtered_data[filtered_data['정류장명'] == bus_stop]
 
+# 필터링된 정보를 표시
+st.write(selected_data)
 
+# 필터링된 정보의 위치를 지도에 표시
+st.map(selected_data)
+
+# 각 정류장의 버스 수를 계산
+bus_counts = filtered_data['정류장명'].value_counts()
+
+# 막대 그래프를 생성
+plt.figure(figsize=(10,5))
+plt.bar(bus_counts.index, bus_counts.values)
+plt.xlabel('정류장명')
+plt.ylabel('버스 수')
+plt.title(f'{city}의 정류장별 버스 수')
+plt.xticks(rotation=90)
+plt.tight_layout()
+
+# 막대 그래프를표시합니다.
+st.pyplot(plt.gcf())
