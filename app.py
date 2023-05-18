725c7e1f9c0ee8251a22a08914ddd43d0fcba246
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-18 21:22:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,40 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.font_manager as fm
+import seaborn as sns
+from datetime import datetime
+
+plt.rcParams['font.family'] = 'NanumGothic'
 
 @st.cache_data
 def load_data():
-    data = pd.read_csv('bus_stop.csv')
-    return data
+    return pd.read_csv("bus_data.csv")
 
 data = load_data()
+data = data.rename(columns={'위도': 'lat', '경도': 'lon'})  # 위도, 경도 컬럼 이름 변경
 
-st.title('대한민국 버스 정류장 정보 조회')
-st.image('bus_image.png') 
-st.header('조회를 시작하세요!')
+st.title("버스 정류장 정보")
+st.markdown("이 웹 앱은 전국의 버스 정류장 정보를 보여줍니다.")
 
-# 도시명 입력
-cities = data['도시명'].unique()
-city = st.sidebar.selectbox('도시를 선택하세요.', cities)
 
-# 정류장명 입력
-bus_stops = data[data['도시명'] == city]['정류장명'].unique()
-bus_stop = st.sidebar.selectbox('정류장을 선택하세요.', bus_stops)
+st.markdown("원하는 데이터를 선택하십시오.")
+selected_columns = st.multiselect("원하는 컬럼을 선택하세요.", data.columns)
 
-# 선택된 도시 정류장에 해당하는 정보를 필터
-filtered_data = data[(data['도시명'] == city) & (data['정류장명'] == bus_stop)]
+if selected_columns:
+    st.dataframe(data[selected_columns])
+else:
+    st.dataframe(data)
 
-# 필터링 정보를 표시
-if st.button('조회'):
-    st.subheader(f"{city}의 {bus_stop} 정보")
-    st.write(filtered_data)
 
-# 필터링된 정보의 위치를 지도에 표시
-if st.checkbox('지도에 표시'):
-    st.map(filtered_data[['위도', '경도']])
+st.subheader("도시별 정류장 수")
+city_counts = data['도시'].value_counts().reset_index()
+city_counts.columns = ['도시', '정류장 수']
+fig, ax = plt.subplots()
+sns.barplot(x='도시', y='정류장 수', data=city_counts, ax=ax)
+plt.xticks(rotation=45)
+st.pyplot(fig)
 
-# 도시별 정류장 수 바 차트
-if st.checkbox('도시별 정류장 수 표시'):
-    city_counts = data['도시명'].value_counts()
-    fig, ax = plt.subplots()
-    city_counts.plot(kind='bar', ax=ax)
-    ax.set_title('도시별 정류장 수')
-    st.pyplot(fig)
 
-# linechart 생성
-if st.checkbox('도시별 최신 정보수집일시'):
-    time_df = data.groupby('도시명')['정보수집일시'].max()
-    st.line_chart(time_df)
+st.subheader("정류장 위치")
+st.map(data)
