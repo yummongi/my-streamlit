3190427ff1e371ff75d36fead757da07d4953f6b
yummongi <101652932+yummongi@users.noreply.github.com>
2023-05-18 21:42:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
 
 @st.cache_data
 def load_data():
@@ -12,21 +14,29 @@ data = load_data()
 st.title('대한민국 버스 정류장 정보 조회')
 st.markdown('버스정보시스템(BIS)이 구축된 지자체 중 국가대중교통정보센터(TAGO)와 연계된 139개 지자체의 버스정류장 위치정보 데이터입니다.')
 
+bus_stop_name = st.text_input("정류장명을 입력하세요.")
+if bus_stop_name:
+    bus_stop_data = data[data['정류장명'] == bus_stop_name]
+    if not bus_stop_data.empty:
+        st.write(bus_stop_data)
+    else:
+        st.write("해당하는 정류장이 없습니다.")
 
-cities = data['도시명'].unique()
-city = st.selectbox('도시를 선택하세요.', cities)
+if st.checkbox('정류장 간 거리를 계산하시겠습니까?'):
+    bus_stop_1 = st.selectbox('첫 번째 정류장을 선택하세요.', data['정류장명'].unique())
+    bus_stop_2 = st.selectbox('두 번째 정류장을 선택하세요.', data['정류장명'].unique())
+    location_1 = data[data['정류장명'] == bus_stop_1][['latitude', 'longitude']].values[0]
+    location_2 = data[data['정류장명'] == bus_stop_2][['latitude', 'longitude']].values[0]
+    gs = gpd.GeoSeries([Point(location_1), Point(location_2)])
+    distance = gs.distance(gs.shift()).values[1]
+    st.write(f"{bus_stop_1}과(와) {bus_stop_2} 사이의 거리는 약 {distance:.2f}km입니다.")
 
 
-bus_stops = data[data['도시명'] == city]['정류장명'].unique()
-bus_stop = st.selectbox('정류장을 선택하세요.', bus_stops)
-
-filtered_data = data[(data['도시명'] == city) & (data['정류장명'] == bus_stop)]
-
-
-st.write(filtered_data)
-
-st.map(filtered_data)
+city = st.selectbox('정류장 분포를 확인하고 싶은 도시를 선택하세요.', data['도시명'].unique())
+city_data = data[data['도시명'] == city]
+st.map(city_data)
 
 
+st.subheader('도시별 정류장 수')
 city_counts = data['도시명'].value_counts()
 st.bar_chart(city_counts)
